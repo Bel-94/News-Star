d158bdd0de4b4ce28c81ceb3da096d0ad0a26f42
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-05-02 13:11:18 +0300

    add the base.html file and new title

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,6 @@
 from flask import render_template,request,redirect,url_for
+
+import app
 from . import main
 from ..requests import get_movies,get_movie,search_movie
 from .forms import ReviewForm
@@ -10,8 +12,10 @@ from ..models import Review
 @main.route('/')
 def index():
 
-    return render_template('index.html')
+    title = 'Hello! Welcome to the best News Site'
+
+    return render_template('index.html', title=title)
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     app.run(debug=True)
