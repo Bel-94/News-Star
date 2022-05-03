6787d91d124e5cd41a21bd4c371b642bb5028749
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-05-03 07:39:32 +0300

    add the routing

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,requests,redirect,url_for
 
-import app
+import app 
 from . import main
-from ..requests import get_movies,get_movie,search_movie
+from ..requests import get_articles,get_article,search_article
 from .forms import ReviewForm
-from ..models import Review
+from ..models import Article
 
 
 
@@ -12,10 +12,37 @@ from ..models import Review
 @main.route('/')
 def index():
 
+    trending_articles = get_articles('trending')
+    upcoming_article = get_articles('upcoming')
+    available_article = get_articles('available')
+
     title = 'Hello! Welcome to the best News Site'
 
-    return render_template('index.html', title=title)
 
+    search_article = requests.args.get('article_query')
+
+    if search_article:
+        return redirect(url_for('search', article_name=search_article))
+    else:
+
+        return render_template('index.html', title=title, trending=trending_articles, upcoming=upcoming_article, available=available_article)
+
+@main.route('/article/<int:article_id>')
+def article(article_id):
+
+    article = get_article(article_id)
+    title = f'{article.title}'
+
+    return render_template('article.html', title=title, article=article )
+
+@main.route('/search/<article_name>')
+def search(article_name):
+
+    article_name_list = article_name.split(" ")
+    article_name_format = "+".join(article_name_list)
+    searched_articles = search_article(article_name_format)
+    title = f'search results for {article_name}'
+    return render_template('search.html', articles=searched_articles )
 
 if __name__ == '__main__':
     app.run(debug=True)
